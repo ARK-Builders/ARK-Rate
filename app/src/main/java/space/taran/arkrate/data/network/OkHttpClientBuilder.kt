2742d32951bc403a448ceae1584d558efbb37f12
mdrlzy <mordeniusss@gmail.com>
2022-11-09 22:36:25 +0600

    Trusting all certificates

--- app/src/main/java/space/taran/arkrate/data/network/OkHttpClientBuilder.kt
+++ app/src/main/java/space/taran/arkrate/data/network/OkHttpClientBuilder.kt
@@ -2,10 +2,18 @@ package space.taran.arkrate.data.network
 
 import android.content.Context
 import android.webkit.WebSettings
+import okhttp3.CipherSuite
+import okhttp3.ConnectionSpec
 import okhttp3.OkHttpClient
+import okhttp3.TlsVersion
 import okhttp3.logging.HttpLoggingInterceptor
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import java.util.Collections
 import javax.inject.Inject
 import javax.inject.Singleton
+import javax.net.ssl.SSLContext
+import javax.net.ssl.X509TrustManager
 
 @Singleton
 class OkHttpClientBuilder @Inject constructor(val context: Context) {
@@ -13,6 +21,24 @@ class OkHttpClientBuilder @Inject constructor(val context: Context) {
         val agent = WebSettings.getDefaultUserAgent(context)
         val logInterceptor = HttpLoggingInterceptor()
         logInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        val manager = object : X509TrustManager {
+            override fun checkClientTrusted(
+                chain: Array<out X509Certificate>?,
+                authType: String?
+            ) {
+            }
+
+            override fun checkServerTrusted(
+                chain: Array<out X509Certificate>?,
+                authType: String?
+            ) {
+            }
+
+            override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+        }
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, arrayOf(manager), SecureRandom())
+
         val client = OkHttpClient.Builder()
             .addNetworkInterceptor { chain ->
                 chain.proceed(
@@ -23,6 +49,8 @@ class OkHttpClientBuilder @Inject constructor(val context: Context) {
                 )
             }
             .addInterceptor(logInterceptor)
+            .sslSocketFactory(sslContext.socketFactory, manager)
+            .hostnameVerifier { _, _ -> true }
             .build()
 
         return client
