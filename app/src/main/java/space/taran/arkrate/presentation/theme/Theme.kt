90c59963c233029c4ded241cd8d384cc6142dc53
mdrlzy <mordeniusss@gmail.com>
2023-06-08 02:03:02 +0600

    Disable dark theme

--- app/src/main/java/space/taran/arkrate/presentation/theme/Theme.kt
+++ app/src/main/java/space/taran/arkrate/presentation/theme/Theme.kt
@@ -29,14 +29,14 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun ARKRateTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+//    val colors = if (darkTheme) {
+//        DarkColorPalette
+//    } else {
+//        LightColorPalette
+//    }
 
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
