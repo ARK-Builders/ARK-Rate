9b8a3defcd50d6aad065a5dd086536ac0cb9015e
Hieu Vu <star.hieu.99@gmail.com>
2024-07-06 14:20:29 +0700

    Extract composable

---
+++ app/src/main/java/dev/arkbuilders/rate/presentation/quick/glancewidget/QuickPairItem.kt
@@ -0,0 +1,68 @@
+package dev.arkbuilders.rate.presentation.quick.glancewidget
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.glance.GlanceModifier
+import androidx.glance.Image
+import androidx.glance.ImageProvider
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.Row
+import androidx.glance.layout.padding
+import androidx.glance.layout.size
+import androidx.glance.text.FontWeight
+import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
+import dev.arkbuilders.rate.data.CurrUtils
+import dev.arkbuilders.rate.presentation.quick.QuickDisplayPair
+import dev.arkbuilders.rate.presentation.theme.ArkColor
+import dev.arkbuilders.rate.presentation.utils.IconUtils
+
+@Composable
+fun QuickPairItem(
+    quick: QuickDisplayPair,
+    context: Context
+) {
+    Row(modifier = GlanceModifier.padding(vertical = 2.dp)) {
+        Image(
+            modifier = GlanceModifier.size(24.dp),
+            provider = ImageProvider(
+                IconUtils.iconForCurrCode(
+                    context,
+                    quick.pair.from
+                )
+            ),
+            contentDescription = null
+        )
+        Image(
+            modifier = GlanceModifier.size(16.dp).padding(start = (-8).dp),
+            provider = ImageProvider(
+                IconUtils.iconForCurrCode(
+                    context,
+                    quick.to.first().code
+                )
+            ),
+            contentDescription = null,
+        )
+        Column(
+            verticalAlignment = Alignment.Vertical.CenterVertically
+        ) {
+            Text(
+                text = "${quick.pair.from} to ${quick.pair.to.joinToString(", ")}",
+                style = TextStyle(
+                    color = ColorProvider(ArkColor.TextPrimary),
+                    fontWeight = FontWeight.Medium
+                )
+            )
+            Text(
+                text = "${CurrUtils.prepareToDisplay(quick.pair.amount)} ${quick.pair.from} = " +
+                        "${CurrUtils.prepareToDisplay(quick.to.first().value)} ${quick.to.first().code}",
+                style = TextStyle(
+                    color = ColorProvider(ArkColor.TextTertiary),
+                )
+            )
+        }
+    }
+}
