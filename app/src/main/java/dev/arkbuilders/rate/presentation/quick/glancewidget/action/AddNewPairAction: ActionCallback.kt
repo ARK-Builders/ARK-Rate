041e09e41f3308629e33a7f77d38cf21580841c0
Hieu Vu <star.hieu.99@gmail.com>
2024-08-16 22:32:43 +0700

    Create event handler in widget

---
+++ app/src/main/java/dev/arkbuilders/rate/presentation/quick/glancewidget/action/AddNewPairAction: ActionCallback.kt
@@ -0,0 +1,25 @@
+package dev.arkbuilders.rate.presentation.quick.glancewidget.action
+
+import android.content.Context
+import android.content.Intent
+import androidx.glance.GlanceId
+import androidx.glance.action.ActionParameters
+import androidx.glance.appwidget.action.ActionCallback
+import dev.arkbuilders.rate.presentation.quick.glancewidget.QuickPairsWidgetReceiver
+
+class AddNewPairAction : ActionCallback {
+    override suspend fun onAction(
+        context: Context,
+        glanceId: GlanceId,
+        parameters: ActionParameters
+    ) {
+        val intent = Intent(context, QuickPairsWidgetReceiver::class.java).apply {
+            action = ADD_NEW_PAIR
+        }
+        context.sendBroadcast(intent)
+    }
+
+    companion object {
+        const val ADD_NEW_PAIR = "QUICK_PAIRS_WIDGET_NEW_PAIR"
+    }
+}
