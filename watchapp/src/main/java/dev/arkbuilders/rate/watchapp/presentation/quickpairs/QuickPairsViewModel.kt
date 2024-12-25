1bbef9acf6e7008df10a130bc58aa2aa46bbb60c
Hieu Vu <star.hieu.99@gmail.com>
2024-12-25 22:45:20 +0700

    Create viewmodel with fake data

---
+++ watchapp/src/main/java/dev/arkbuilders/rate/watchapp/presentation/quickpairs/QuickPairsViewModel.kt
@@ -0,0 +1,97 @@
+package dev.arkbuilders.rate.watchapp.presentation.quickpairs
+
+import androidx.lifecycle.ViewModel
+import dev.arkbuilders.rate.core.domain.model.Amount
+import dev.arkbuilders.rate.feature.quick.domain.model.QuickPair
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import java.math.BigDecimal
+import java.time.OffsetDateTime
+
+class QuickPairsViewModel : ViewModel() {
+
+    private val _quickPairs: MutableStateFlow<List<QuickPair>> = MutableStateFlow(listOf())
+    val quickPairs: StateFlow<List<QuickPair>> = _quickPairs
+
+    init {
+        val a = listOf(
+            QuickPair(
+                id = 1,
+                from = "BTC",
+                amount = BigDecimal.valueOf(1.2),
+                to = listOf(
+                    Amount("USD", BigDecimal.valueOf(12.0)),
+                    Amount("EUR", BigDecimal.valueOf(12.0))
+                ),
+                calculatedDate = OffsetDateTime.now(),
+                pinnedDate = null,
+                group = null
+            ),
+            QuickPair(
+                id = 1,
+                from = "BTC",
+                amount = BigDecimal.valueOf(1.2),
+                to = listOf(
+                    Amount("USD", BigDecimal.valueOf(12.0)),
+                    Amount("EUR", BigDecimal.valueOf(12.0))
+                ),
+                calculatedDate = OffsetDateTime.now(),
+                pinnedDate = null,
+                group = null
+            ),
+
+            QuickPair(
+                id = 1,
+                from = "BTC",
+                amount = BigDecimal.valueOf(1.2),
+                to = listOf(
+                    Amount("USD", BigDecimal.valueOf(12.0)),
+                    Amount("EUR", BigDecimal.valueOf(12.0))
+                ),
+                calculatedDate = OffsetDateTime.now(),
+                pinnedDate = null,
+                group = null
+            ),
+
+            QuickPair(
+                id = 1,
+                from = "BTC",
+                amount = BigDecimal.valueOf(1.2),
+                to = listOf(
+                    Amount("USD", BigDecimal.valueOf(12.0)),
+                    Amount("EUR", BigDecimal.valueOf(12.0))
+                ),
+                calculatedDate = OffsetDateTime.now(),
+                pinnedDate = null,
+                group = null
+            ),
+            QuickPair(
+                id = 1,
+                from = "BTC",
+                amount = BigDecimal.valueOf(1.2),
+                to = listOf(
+                    Amount("USD", BigDecimal.valueOf(12.0)),
+                    Amount("EUR", BigDecimal.valueOf(12.0))
+                ),
+                calculatedDate = OffsetDateTime.now(),
+                pinnedDate = null,
+                group = null
+            ),
+
+            QuickPair(
+                id = 1,
+                from = "BTC",
+                amount = BigDecimal.valueOf(1.2),
+                to = listOf(
+                    Amount("USD", BigDecimal.valueOf(12.0)),
+                    Amount("EUR", BigDecimal.valueOf(12.0))
+                ),
+                calculatedDate = OffsetDateTime.now(),
+                pinnedDate = null,
+                group = null
+            )
+        )
+        _quickPairs.value = a
+    }
+
+}
