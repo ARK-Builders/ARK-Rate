03e816fa2109634637b26be72b52cb695397b451
Hieu Vu <star.hieu.99@gmail.com>
2025-01-07 22:22:59 +0700

    Tweak UI

--- watchapp/src/main/java/dev/arkbuilders/rate/watchapp/presentation/quickpairs/QuickPairsScreen.kt
+++ watchapp/src/main/java/dev/arkbuilders/rate/watchapp/presentation/quickpairs/QuickPairsScreen.kt
@@ -2,11 +2,14 @@ package dev.arkbuilders.rate.watchapp.presentation.quickpairs
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.material.Text
 import dev.arkbuilders.rate.watchapp.presentation.quickpairs.composables.QuickPairItem
 import dev.arkbuilders.rate.watchapp.presentation.quickpairs.composables.QuickPairsEmpty
 
@@ -25,6 +28,13 @@ fun QuickPairsScreen(
             modifier = modifier.fillMaxSize(),
             contentPadding = PaddingValues(4.dp)
         ) {
+            item {
+                Text(
+                    modifier = modifier.fillMaxWidth(),
+                    text = "Options",
+                    textAlign = TextAlign.Center
+                )
+            }
             items(quickPairsList.size, key = null) { idx ->
                 QuickPairItem(quick = quickPairsList[idx], onClick = onNavigateToAdd)
             }
