0c5a42a72ad72748b7f52e4d2c622619414bd261
Hieu Vu <star.hieu.99@gmail.com>
2025-01-07 21:52:12 +0700

    Improve list effect

--- watchapp/src/main/java/dev/arkbuilders/rate/watchapp/presentation/quickpairs/QuickPairsScreen.kt
+++ watchapp/src/main/java/dev/arkbuilders/rate/watchapp/presentation/quickpairs/QuickPairsScreen.kt
@@ -2,12 +2,11 @@ package dev.arkbuilders.rate.watchapp.presentation.quickpairs
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
 import dev.arkbuilders.rate.watchapp.presentation.quickpairs.composables.QuickPairItem
 import dev.arkbuilders.rate.watchapp.presentation.quickpairs.composables.QuickPairsEmpty
 
@@ -21,11 +20,12 @@ fun QuickPairsScreen(
     if (quickPairsList.isEmpty()) {
         QuickPairsEmpty(modifier = modifier.fillMaxSize())
     } else {
-        LazyColumn(modifier = modifier.fillMaxSize(),
+        ScalingLazyColumn(
+            modifier = modifier.fillMaxSize(),
             contentPadding = PaddingValues(4.dp)
         ) {
-            items(quickPairsList) { quickPair ->
-                QuickPairItem(quick = quickPair)
+            items(quickPairsList.size, key = null) { idx ->
+                QuickPairItem(quick = quickPairsList[idx])
             }
         }
     }
