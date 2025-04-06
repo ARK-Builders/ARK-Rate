455ea99370729ee03a31d848894483b503b85d56
Hieu Vu <star.hieu.99@gmail.com>
2025-04-06 11:03:27 +0700

    Upgrade SDK version

--- cryptoicons/build.gradle.kts
+++ cryptoicons/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "dev.arkbuilders.rate.cryptoicons"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 26
@@ -24,11 +24,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
--- feature/pairalert/build.gradle.kts
+++ feature/pairalert/build.gradle.kts
@@ -34,11 +34,11 @@ android {
         kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
