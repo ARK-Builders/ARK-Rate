81773340ee4db3f05a9688854cfcd01986f86560
Hieu Vu <star.hieu.99@gmail.com>
2025-04-20 09:39:24 +0700

    Remove uneeded plugin import

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.ksp)
-    alias(libs.plugins.compose.compiler) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.1" apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
