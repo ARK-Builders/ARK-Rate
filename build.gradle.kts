89b76adddf89c4f137a3830421c7f5e53d32ba60
Hieu Vu <star.hieu.99@gmail.com>
2025-05-01 20:21:44 +0700

    Upgrade to SDK 35, Kotlin 2.0, JVM 21 and other libraries, apply Compose Plugin (#142)
    * Upgrade SDK version
    
    * Upgrade kotlin version, replace compose option with compose plugin. Upgrade libraries
    
    * Update action
    
    * Add PR template
    
    * Remove uneeded plugin import

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.3" apply false
+    id("com.android.application") version "8.9.1" apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
