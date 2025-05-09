bccf5631d3ebacfb2f2bf960dd79b39f2e5b9e28
mdrlzy <mordeniusss@gmail.com>
2025-05-09 18:00:11 +0600

    Bump app version to 7(2.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.arkbuilders.rate"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.compileSdk.get().toInt()
-        versionCode = 6
-        versionName = "2.0.2"
+        versionCode = 7
+        versionName = "2.1"
         setProperty("archivesBaseName", "ark-rate")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
