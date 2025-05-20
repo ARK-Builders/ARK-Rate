bef74435e5549195bde1a261c75a246d6599b9f4
Kirill Taran <kirill@taran.space>
2025-05-20 23:17:41 +0900

    Fix linter

--- core/data/src/main/java/dev/arkbuilders/rate/core/data/network/api/CryptoAPI.kt
+++ core/data/src/main/java/dev/arkbuilders/rate/core/data/network/api/CryptoAPI.kt
@@ -1,9 +1,9 @@
+@Suppress("ktlint:standard:max-line-length")
 package dev.arkbuilders.rate.core.data.network.api
 
 import dev.arkbuilders.rate.core.data.network.dto.CryptoRateResponse
 import retrofit2.http.GET
 
-@Suppress("ktlint:standard:max-line-length")
 interface CryptoAPI {
     @GET("/ARK-Builders/cache-exchange-rates/refs/heads/joshwhittick-patch-1-1/crypto-rates-JW.json")
     suspend fun getCryptoRates(): List<CryptoRateResponse>
