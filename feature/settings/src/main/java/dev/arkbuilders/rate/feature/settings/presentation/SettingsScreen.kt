c60205e3271f233a6057bf97c261cf267a36e167
mdrlzy <mordeniusss@gmail.com>
2025-05-24 00:27:42 +0600

    Fix settings window insets

--- feature/settings/src/main/java/dev/arkbuilders/rate/feature/settings/presentation/SettingsScreen.kt
+++ feature/settings/src/main/java/dev/arkbuilders/rate/feature/settings/presentation/SettingsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
@@ -55,6 +56,7 @@ fun SettingsScreen(navigator: DestinationsNavigator) {
 
     Scaffold(
         modifier = Modifier.statusBarsPadding().imePadding(),
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
     ) {
         Box(modifier = Modifier.padding(it)) {
             Content(
