778d74e0f71c30f3c3e13cdb04da8f0e380291f2
Hieu Vu <star.hieu.99@gmail.com>
2025-09-06 21:21:43 +0700

    Implement search screen

---
+++ watchapp/src/main/java/dev/arkbuilders/rate/watchapp/presentation/search/SearchViewModel.kt
@@ -0,0 +1,17 @@
+package dev.arkbuilders.rate.watchapp.presentation.search
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.arkbuilders.rate.core.domain.usecase.SearchUseCase
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+//    private val searchUseCase: SearchUseCase,
+): ViewModel() {
+
+    init {
+
+    }
+
+}
